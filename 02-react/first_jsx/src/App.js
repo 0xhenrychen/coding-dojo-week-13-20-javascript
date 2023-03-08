6e249173a591669d3b235d16cb975f3894862762
Henry Chen <henrychen14505@gmail.com>
2023-03-08 14:48:32 -0800

    first_jsx assignment completed

---
+++ 02-react/first_jsx/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header_color">Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul className="list">
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
