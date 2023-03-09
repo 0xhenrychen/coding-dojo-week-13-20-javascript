40e2aa0994dfe68f376962232d3c20f220f2de8a
Henry Chen <henrychen14505@gmail.com>
2023-03-09 13:12:24 -0800

    putting_it_together assignment completed

--- 02-react/functional_components/prop_it_up/src/components/PersonCard.js
+++ 02-react/functional_components/prop_it_up/src/components/PersonCard.js
@@ -6,6 +6,7 @@ const PersonCard = (props) => {
             <h1>{props.lastName}, {props.firstName}</h1>
             <h2>Age: {props.age}</h2>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick={() => alert("This button has been clicked!")}>Click Me</button>
         </div>
     )
 }
---
+++ 02-react/functional_components/putting_it_together/src/components/PersonCard.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+
+const PersonCard = (props) => {
+    const [age, setAge] = useState({clickCount: props.age});
+
+    const handleClick = () => {setAge({clickCount: age.clickCount + 1});
+    }
+    
+    
+    return (
+        <div>
+            <h1>{props.lastName}, {props.firstName}</h1>
+            <h2>Age: {age.clickCount}</h2>
+            <p>Hair Color: {props.hairColor}</p>
+            <button onClick = {handleClick}>Birthday Button for {props.firstName} {props.lastName}</button>
+        </div>
+    )
+}
+export default PersonCard;
